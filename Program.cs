8c109cf5def20aa543046e574a8a37f1b73094e2
Olivia Persson <Olivia__P@hotmail.com>
2020-10-26 16:40:05 +0100

    New method that checks if variable contains 12 digits

--- Program.cs
+++ Program.cs
@@ -16,7 +16,22 @@ namespace Validation_Civic_Number
             Console.WriteLine("Skriv in ett 12-siffrigt personnummer enligt följande YYYYMMDDnnnc: ");
             userInput = Console.ReadLine();
 
+            //Calls method NumberOfDigits and sends in string value
+            NumberOfDigits(userInput);
+
+            //Stop
             Console.ReadKey();
+        }
+        //--------------------------------------------------------
+        //Method that checks that the number of digits is correct
+        //--------------------------------------------------------
+        static void NumberOfDigits(string civicNumber)
+        {
+            //Expression that checks if the variable contains 12 digits
+            if (civicNumber.Length < 12 || civicNumber.Length > 12)
+            {
+                Console.WriteLine("Du har ej angett 12st siffror.");
+            }
         }
     }
 }
