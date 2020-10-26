f693036bc2bdb27285324b212cc90f8f8a5af166
Olivia Persson <Olivia__P@hotmail.com>
2020-10-26 15:17:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation_Civic_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+
+            //Ask user for string input
+            Console.WriteLine("Skriv in ett 12-siffrigt personnummer YYYYMMDDnnnc: ");
+            userInput = Console.ReadLine();
+
+
+
+        }
+    }
+}
