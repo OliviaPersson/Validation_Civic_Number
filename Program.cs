f6587caeb70e4af13b2b957a7394aef7e05a1aa3
Olivia Persson <Olivia__P@hotmail.com>
2020-10-26 17:06:54 +0100

    New method that checks correct year

--- Program.cs
+++ Program.cs
@@ -11,13 +11,19 @@ namespace Validation_Civic_Number
         static void Main(string[] args)
         {
             string userInput = "";
+            int year = 0;
            
             //Ask user for input
             Console.WriteLine("Skriv in ett 12-siffrigt personnummer enligt följande YYYYMMDDnnnc: ");
             userInput = Console.ReadLine();
 
+            //Takes out year, converts and places in an int variable
+            year = int.Parse(userInput.Substring(0, 4));
+
             //Calls method NumberOfDigits and sends in string value
             NumberOfDigits(userInput);
+            //Calls method CorrectYear and send in int variable
+            CorrectYear(year);
 
             //Stop
             Console.ReadKey();
@@ -31,6 +37,16 @@ namespace Validation_Civic_Number
             if (civicNumber.Length < 12 || civicNumber.Length > 12)
             {
                 Console.WriteLine("Du har ej angett 12st siffror.");
+            }
+        }
+        //--------------------------------------------------------
+        //Method that checks that the correct year has been entered
+        //--------------------------------------------------------
+        static void CorrectYear(int year)
+        {
+            if (year < 1753 || year > 2020)
+            {
+                Console.WriteLine("Du har angett ett felaktigt årtal.");
             }
         }
     }
