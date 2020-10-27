9f7025e911b4067918e0e0676c5520f17814ef67
Olivia Persson <Olivia__P@hotmail.com>
2020-10-27 15:18:46 +0100

    New first draft to check days in months

--- Program.cs
+++ Program.cs
@@ -28,29 +28,76 @@ namespace Validation_Civic_Number
             year = int.Parse(userInput.Substring(0, 4));
            
             //Checks if userinput is 00
-            if(userInput.Substring(4, 2) != "00")
+            if (userInput.Substring(4, 2) != "00")
             {
                 //Takes out month, remove 0 if first number, converts and places in an int variable
                 month = int.Parse(userInput.Substring(4, 2).TrimStart('0'));
             } 
          
-            //Takes out day, convert and store in variable
-            day = int.Parse(userInput.Substring(6, 2));
+            if (userInput.Substring(6, 2) != "00")
+            {
+                //Takes out day, convert and store in variable
+                day = int.Parse(userInput.Substring(6, 2).TrimStart('0'));
+            }
+           
             //Takes out last integer in birtnumber and places in variable
             birthNumber = int.Parse(userInput.Substring(10, 1));
 
-            //Calls method NumberOfDigits and sends in string value
+            //Method calls
             NumberOfDigits(userInput);
-            //Calls method CorrectYear and send in int variable
             CorrectYear(year);
-            // Calls method checkLeapYear and stores true or false in variable
+            checkMonth(month);
             isLeapYear = checkLeapYear(year);
-            // Calls method checkGender and stores in variable
             gender = GetGender(birthNumber);
 
-            checkMonth(month);
-            
-
+            // A first draft of arrays, for loop and if-set to check days in months
+            int[] longMonth = new int[] { 1, 3, 5, 7, 8, 10, 12 };
+            int[] shortMonth = new int[] { 4, 6, 9, 11 };
+           
+            //Loop that checks months array with 31 days
+            for (int i=0; i < longMonth.Length; i++)
+            {
+                if (longMonth[i] == month)
+                {
+                    if (day > 31)
+                    {
+                        Console.WriteLine("Felaktig dag har angetts");
+                    }
+                }
+            }
+            //Loop that checks months array with 30 days
+            for (int i = 0; i < shortMonth.Length; i++)
+            {
+                if (shortMonth[i] == month)
+                {
+                    if (day > 30)
+                    {
+                        Console.WriteLine("Felaktig dag har angetts");
+                    }
+                }
+            }
+            //Check month if year is not leap year
+            if (isLeapYear == false)
+            {
+                if (month == 2)
+                {
+                    if (day > 28)
+                    {
+                        Console.WriteLine("Felaktig dag har angetts");
+                    }
+                }
+            }
+            //Check month if year is leap year
+            else
+            {
+                if (month == 2)
+                {
+                    if (day > 29)
+                    {
+                        Console.WriteLine("Felaktig dag har angetts");
+                    }
+                }
+            }
             //Stop
             Console.ReadKey();
         }
@@ -73,6 +120,16 @@ namespace Validation_Civic_Number
             if (year < 1753 || year > 2020)
             {
                 Console.WriteLine("Du har angett ett felaktigt årtal.");
+            }
+        }
+        //--------------------------------------------------------
+        //Method that checks month
+        //--------------------------------------------------------
+        static void checkMonth(int month)
+        {
+            if (month < 1 || month > 12)
+            {
+                Console.WriteLine("Du har angett en felaktig månad");
             }
         }
         //--------------------------------------------------------
@@ -109,14 +166,7 @@ namespace Validation_Civic_Number
             }
         } 
         //--------------------------------------------------------
-        //Method that checks month
+        //Method that checks valid days in months
         //--------------------------------------------------------
-        static void checkMonth(int month)
-        {
-            if(month < 1 || month > 12)
-            {
-                Console.WriteLine("Du har angett en felaktig månad");
-            }
-        }
     }
 }
