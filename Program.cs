73cbf75b33664d7557fd4b600a4f05da7771fb89
Olivia Persson <Olivia__P@hotmail.com>
2020-10-29 11:09:05 +0100

    Comment adjustments

--- Program.cs
+++ Program.cs
@@ -30,24 +30,20 @@ namespace Validation_Civic_Number
 
             //Takes out year, converts and places in an int variable
             year = int.Parse(userInput.Substring(0, 4));
-           
-            //Checks if userinput is 00
+            //Checks if userinput is 00, and if keeps value 0 in variable month
             if (userInput.Substring(4, 2) != "00")
             {
-                //Takes out month, remove 0 if first number, converts and places in an int variable
+                //Takes out month, if first number is 0 remove 0, converts and places in an int variable
                 month = int.Parse(userInput.Substring(4, 2).TrimStart('0'));
             } 
-         
             if (userInput.Substring(6, 2) != "00")
             {
-                //Takes out day, convert and store in variable
                 day = int.Parse(userInput.Substring(6, 2).TrimStart('0'));
             }
-           
             //Takes out last integer in birtnumber and places in variable
             birthNumber = int.Parse(userInput.Substring(10, 1));
 
-            //Method calls that returns true, false and a string and stores in variables
+            //Method calls that returns true, false or a string depending on correct input and stores in variables
             correctNumberOfDigits = NumberOfDigits(userInput);
             correctYear = CorrectYear(year);
             correctMonth = CheckMonth(month);
@@ -60,12 +56,11 @@ namespace Validation_Civic_Number
             {
                 Console.WriteLine("Personnumret är korrekt och du är {0} (juridiskt)", gender);
             }
-       
-            //Stop
             Console.ReadKey();
         }
         //--------------------------------------------------------
         // Method that checks that the number of digits is correct
+        // Returns true if correct and false if not
         //--------------------------------------------------------
         static bool NumberOfDigits(string civicNumber)
         {
@@ -79,6 +74,7 @@ namespace Validation_Civic_Number
         }
         //--------------------------------------------------------
         // Method that checks that the correct year has been entered
+        // Returns true if correct and false if not
         //--------------------------------------------------------
         static bool CorrectYear(int year)
         {
@@ -90,7 +86,8 @@ namespace Validation_Civic_Number
             return true;
         }
         //--------------------------------------------------------
-        // Method that checks month
+        // Method that checks if a correct month is entered 
+        // Returns true if correct and false if not
         //--------------------------------------------------------
         static bool CheckMonth(int month)
         {
@@ -103,6 +100,7 @@ namespace Validation_Civic_Number
         }
         //--------------------------------------------------------
         // Method that checks if a year is a leap year
+        // Returns true if correct and false if not
         //--------------------------------------------------------
         static bool CheckLeapYear(int year)
         {
@@ -122,6 +120,7 @@ namespace Validation_Civic_Number
         }
         //--------------------------------------------------------
         // Method that checks gender
+        // Returns string of gender
         //--------------------------------------------------------
         static string GetGender(int birthNumber)
         {
@@ -133,26 +132,26 @@ namespace Validation_Civic_Number
             {
                 return "man";
             }
-        } 
+        }
         //--------------------------------------------------------
         // Method that checks valid days in months
+        // Returns true if correct and false if not
         //--------------------------------------------------------
         static bool CheckValidDaysInMonth(bool isLeapYear, int month, int day)
         {
-            //Months divided into variables based on days
+            //Months divided into variables based on number of days. 30 and 31, respectively
             int[] longMonth = new int[] { 1, 3, 5, 7, 8, 10, 12 };
             int[] shortMonth = new int[] { 4, 6, 9, 11 };
 
-            //Two method calls that return true or false and stores in variables
+            //Two method calls that return true or false depending  on input and stores in variables
             bool correctLongMonth = LongMonthShortMonth(longMonth, month, day, 31);
             bool correctShortMonth = LongMonthShortMonth(shortMonth, month, day, 30);
 
-            //checks if one of the two variables is false and if then return false
+            //Checks if one of the two variables is false and if then return false
             if(correctLongMonth == false || correctShortMonth == false)
             {
                 return false;
             }
-
             //Check month if year is not leap year
             if (isLeapYear == false)
             {
@@ -162,7 +161,7 @@ namespace Validation_Civic_Number
                     //If day is greater than 28 or less than 1 print to cmd
                     if (day > 28 || day < 1)
                     {
-                        Console.WriteLine("Felaktig dag har angetts. Vänlig försök igen.");
+                        Console.WriteLine("Felaktig dag har angetts. Det är ej skottår. Vänlig försök igen.");
                         return false;
                     }
                 }
@@ -183,13 +182,14 @@ namespace Validation_Civic_Number
         }
         //--------------------------------------------------------
         // Method that checks LongMonth and shortMonth
+        // Returns true if correct and false if not
         //--------------------------------------------------------
         static bool LongMonthShortMonth(int[] array, int month, int day, int daysInMonth)
         {
             //Loop to go through the array
             for (int i = 0; i < array.Length; i++)
             {
-                //Checks if number i i position is equal to month
+                //Checks if number in i position is equal to month
                 if (array[i] == month)
                 {
                     //If day is greater than daysInMonth or less than 1 print to cmd
